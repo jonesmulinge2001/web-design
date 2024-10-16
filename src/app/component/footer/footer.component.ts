7941d33edf91b8d11e0fce8836c9d0f085337610
Janathan <jones@gmail.com>
2024-10-16 11:20:39 +0300

    second commit

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGoogle, faTwitter, faFacebook, faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faGoogle = faGoogle;
+  faTwitter = faTwitter;
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
 }
